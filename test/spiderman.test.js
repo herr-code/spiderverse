29d29095dca4aff334e8bbd23151f535cb0453f8
herr-code <herrwebj@gmail.com>
2022-04-28 23:16:29 -0500

    Parte 3 Step 1 Write a test for the next bit of functionality you want to add

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,19 @@
-describe("Test Suite Dummy Description", () => {
-  test('Case 1 Dummy', () => {
-    const resultOfSomething = 1 + 2
-    expect(resultOfSomething).toBe(18);
+/*
+	Requerimiento
+	Crea una clase spiderman que tenga como atributos los siguientes campos: 
+
+	nombre, edad, actor que lo interpretó, número de películas en las que apareció, 
+	estudio de cine.
+*/
+describe("Unit Tests for Spiderman class", () => {
+  test('1) Create an spiderman object', () => {
+    //Aqui escribimos el código que queremos usar tal cuál 
+    //Quiero poder instanciar un objeto Spiderman con esta información
+    const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+    expect(andrewGarfield.name).toBe("Spiderman Sony");
+    expect(andrewGarfield.age).toBe(31);
+    expect(andrewGarfield.actor).toBe("Andrew Garfield");
+    expect(andrewGarfield.movies).toBe(31);
+    expect(andrewGarfield.studio).toBe("Sony");
   });
 })
